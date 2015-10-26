0b0be9a48e81d04200f96612118b1a91ba23f18e
Mike Barnard <michael.d.barnard@gmail.com>
2015-10-26 15:01:59 -0400

    Chapter 4 complete

--- leaderboard.js
+++ leaderboard.js
@@ -1,18 +1,21 @@
-PlayersList = new Mongo.Collection('players');
+PlayersList = new Mongo.Collection('players')
 
 if(Meteor.isClient){
   Template.leaderboard.helpers({
     'player': function(){
       return PlayersList.find();
     },
-    'otherHelperFunction': function(){
-      return "Some other function"
+    'selectedClass' : function(){
+      if ( Session.get('selectedPlayer') == this._id ) {
+        return "selected"
+      }
     }
   });
 
   Template.leaderboard.events({
     'click .player' : function() {
-      console.log("You clicked an .player");  
+      var playerId = this._id
+      Session.set('selectedPlayer', playerId)
     }
   });
 }
