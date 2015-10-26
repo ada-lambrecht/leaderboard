8978c2baaba06a546a9f7d7587ed2fec20e7955b
Mike Barnard <michael.d.barnard@gmail.com>
2015-10-26 14:35:51 -0400

    Chapter 2 complete

--- leaderboard.js
+++ leaderboard.js
@@ -1,23 +1,16 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+PlayersList = new Mongo.Collection('players');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'otherHelperFunction': function(){
+      return "Some other function"
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if(Meteor.isServer){
+    // this code only runs on the server
 }
